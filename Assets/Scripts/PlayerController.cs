e26bee56aae2b0fa33fbda759b62ec96d3844d36
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-01-09 17:47:17 +0000

    feat: Add Obstacles and Routes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
+    public Transform routeFather;
+    private int indexChildren;
+    private Vector3 destination;
+
+    private void Start () {
+        destination = routeFather.GetChild(indexChildren).position;
+        GetComponent<NavMeshAgent>().SetDestination(destination);
+    }
+
     private void Update() {
         if (Input.GetButtonDown("Firel")) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
